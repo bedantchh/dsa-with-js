7cc4b6d607c53c4a9357a829a484252955102a51
Bedant Chhetri <bedantchhetri7@gmail.com>
2024-08-22 13:00:18 +0530

    shift()

--- 01-Arrays/00-custom-arrays.js
+++ 01-Arrays/00-custom-arrays.js
@@ -19,6 +19,17 @@ class Myarray {
     delete this.data[this.length - 1];
     this.length--;
   }
+
+  shift() {
+    const firstItem = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+    return firstItem;
+  }
 }
 
 const myNewArray = new Myarray();
@@ -30,7 +41,11 @@ myNewArray.push("Banana");
 
 // console.log(myNewArray);
 
-myNewArray.get(0);
+// myNewArray.get(0);
 
-myNewArray.pop();
+// myNewArray.pop();
+// console.log(myNewArray);
+
+console.log(myNewArray);
+console.log(myNewArray.shift());
 console.log(myNewArray);
