0dcba536439107871d17a9a766055ccc1c9735b0
Bedant Chhetri <bedantchhetri7@gmail.com>
2024-08-26 00:23:17 +0530

    using function

--- 02. Arrays/02. reverse-string.js
+++ 02. Arrays/02. reverse-string.js
@@ -3,6 +3,11 @@ let str = "apple";
 let str_arr = str.split("").reverse().join();
 console.log(str_arr);
 
+//easy method with the arrow function
+
+const reversedString = (string) => string.split("").reverse().join();
+console.log(reversedString("mango"));
+
 // with loop
 let str_arr2 = str.split("");
 let reversedStr = [];
