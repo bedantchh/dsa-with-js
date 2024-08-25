9167c2a677634504dd62bae12035a509b05a437b
Bedant Chhetri <bedantchhetri7@gmail.com>
2024-08-26 00:04:00 +0530

    reverse with loop

--- 02. Arrays/02. reverse-string.js
+++ 02. Arrays/02. reverse-string.js
@@ -1,4 +1,12 @@
+// easy method
 let str = "apple";
 let str_arr = str.split("").reverse().join();
-
 console.log(str_arr);
+
+// with loop
+let str_arr2 = str.split("");
+let reversedStr = [];
+for (let i = str_arr2.length - 1; i >= 0; i--) {
+  reversedStr.push(str_arr2[i]);
+}
+console.log(reversedStr.join());
