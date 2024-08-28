1ec6874476f59c2bc73c75bd30ec76fc10450bbf
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-08-28 11:43:41 +0000

    capitalize

--- 02. Arrays/05. capitalize.js
+++ 02. Arrays/05. capitalize.js
@@ -1,12 +1,6 @@
 const capitalize = (str) => {
-    let cap = [];
-    let newWord = str.toLowerCase().split(" ")
-    console.log(newWord)
-    for(let i=0;i<newWord.length;i++){
-        cap.push(newWord[i].toUpperCase())
-    }
-    console.log(cap)
-
+    let newWord = str.toLowerCase().split(" ").map( word => word[0].toUpperCase() + word.slice(1)). join(" ")
+    return newWord
 }
 
-capitalize("hi i am bedant!")
+console.log(capitalize("hello there"))
